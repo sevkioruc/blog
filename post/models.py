dbede494176a07a8b8dce1e87cd871a6f7540b79
Sevki <sevki@Sevki>
2020-02-27 01:19:53 +0300

    create post model

--- post/models.py
+++ post/models.py
@@ -1,1 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class PostModel(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    publishingDate = models.DateTimeField(auto_now=True)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
