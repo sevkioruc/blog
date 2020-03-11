80ab9f1bd0a3f9d6265361f06d83ff5d4246a995
Sevki <sevki@Sevki>
2020-03-12 00:14:42 +0300

    jwt authorization fix

--- blog-frontend/src/services/post.service.ts
+++ blog-frontend/src/services/post.service.ts
@@ -1,29 +1,34 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Post } from 'src/models/post.model';
 
 @Injectable({providedIn: 'root'})
 export class PostService {
      
     private readonly baseUrl = 'http://127.0.0.1:8000/';
+    private requestHeader: HttpHeaders;
 
     constructor(private http: HttpClient) {
+        this.requestHeader = new HttpHeaders({ 
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + localStorage.getItem('token')
+         });
     }
 
     getPosts() {
-        return this.http.get(this.baseUrl + 'api/post/list');
+        return this.http.get(this.baseUrl + 'api/post/list', { headers: this.requestHeader });
     }
 
     deletePost(id: number) {
-        return this.http.delete(this.baseUrl + 'api/post/delete/' + id);
+        return this.http.delete(this.baseUrl + 'api/post/delete/' + id, { headers: this.requestHeader });
     }
 
     createPost(post: Post) {
-        return this.http.post(this.baseUrl + 'api/post/create', post);
+        return this.http.post(this.baseUrl + 'api/post/create', post, { headers: this.requestHeader });
     }
 
     updatePost(post: Post, id: number) {
-        return this.http.put(this.baseUrl + 'api/post/update/' + id, post);
+        return this.http.put(this.baseUrl + 'api/post/update/' + id, post, { headers: this.requestHeader });
     }
 
 }
--- post/api/views.py
+++ post/api/views.py
@@ -1,26 +1,32 @@
 from rest_framework.generics import ListAPIView, RetrieveAPIView, DestroyAPIView, UpdateAPIView, CreateAPIView
 from post.models import PostModel
 from .serializers import PostSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class PostListAPIView(ListAPIView):
     queryset = PostModel.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
 
 class PostDetailAPIView(RetrieveAPIView):
     queryset = PostModel.objects.all()
     serializer_class = PostSerializer
     lookup_field = 'pk'
+    permission_classes = [IsAuthenticated]
 
 class PostDeleteAPIView(DestroyAPIView):
     queryset = PostModel.objects.all()
     serializer_class = PostSerializer
     lookup_field = 'pk'
+    permission_classes = [IsAuthenticated]
 
 class PostUpdateAPIView(UpdateAPIView):
     queryset = PostModel.objects.all()
     serializer_class = PostSerializer
     lookup_field = 'pk'
+    permission_classes = [IsAuthenticated]
 
 class PostCreateAPIView(CreateAPIView):
     queryset = PostModel.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
